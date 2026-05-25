9aff4768b1bbae2fbca4f64b209134ca71b118ba
MOHAMED Kallel <mohamed@helloryse.com>
2026-05-25 08:06:18 +0100

    Fix review: async_added_to_hass / async_will_remove_from_hass

--- custom_components/ryse/cover.py
+++ custom_components/ryse/cover.py
@@ -64,6 +64,7 @@ class RyseCoverEntity(CoverEntity):
 
     async def async_added_to_hass(self) -> None:
         """Run when entity is added to Home Assistant."""
+        await super().async_added_to_hass()
 
         # Register the callback safely
         self._device.update_callback = self._update_position
@@ -73,6 +74,7 @@ class RyseCoverEntity(CoverEntity):
 
     async def async_will_remove_from_hass(self) -> None:
         """Cleanup before entity removal."""
+        await super().async_will_remove_from_hass()
         self._clear_callback()
 
     def _clear_callback(self) -> None:
